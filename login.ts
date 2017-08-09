d39ffac695ed23db9468e835a5b0b4131bd68881
Attila Herbert <herbertatus@gmail.com>
2017-08-09 14:42:19 +0200

    jquery

--- login.ts
+++ login.ts
@@ -1,4 +1,4 @@
-import $ from 'jQuery';
+import $ from 'jquery';
 
 function initGAuth(){
     console.log('assert');
--- main.js
+++
@@ -1,8 +0,0 @@
-/*jslint browser:true*/
-/*jslint esnext:true*/
-/*jslint node:true*/
-/*global $, console: false*/
-
-require("babel-core").transform("login.js", options);
-
-
